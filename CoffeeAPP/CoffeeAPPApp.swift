bc69da3f409a9f11210f4b058f262bbc34bb0195
MacTai <591390744@qq.com>
2020-07-25 16:15:32 +0800

    Initial Commit

---
+++ CoffeeAPP/CoffeeAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAPPApp.swift
+//  CoffeeAPP
+//
+//  Created by Medcon on 2020/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeAPP/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CoffeeAPP
+//
+//  Created by Medcon on 2020/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
